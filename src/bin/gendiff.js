833122f2da24caa82176b6c69c2ffe7f0963f92d
Vadim Novash <novash@tut.by>
2020-06-26 15:17:50 +0300

    add filepath arguments

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,5 +4,6 @@ import program from 'commander';
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format');
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 program.parse(process.argv);
